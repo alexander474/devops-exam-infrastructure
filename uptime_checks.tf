0d82d92669d74b989c3d037d4a2b0ee636b8eebc
Alexander Bredesen <alex474@hotmail.no>
2020-11-16 14:02:59 +0100

    fix string formating error

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -4,7 +4,7 @@ provider "statuscake" {
 
 resource "statuscake_test" "googlecloudruntest" {
   website_name = "exam"
-  website_url = google_cloud_run_service.app.status[0].url + "/stock"
+  website_url = "${google_cloud_run_service.app.status[0].url}/stock"
   test_type = "HTTP"
   check_rate = 300
   contact_group = ["Default"]
