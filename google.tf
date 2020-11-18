ae4d19c33f7abf61e7f7b20a0e8130e0c33eb999
Alexander Bredesen <alex474@hotmail.no>
2020-11-18 18:21:15 +0100

    changed container image id

--- google.tf
+++ google.tf
@@ -7,7 +7,7 @@ resource "google_cloud_run_service" "app" {
   template {
     spec {
       containers {
-        image = "gcr.io/${var.project_id}/app:705527145d97452b125f795c2e27da056832d800"
+        image = "gcr.io/${var.project_id}/app@sha256:705527145d97452b125f795c2e27da056832d800"
         env {
           name = "LOGZ_TOKEN"
           value = var.logz_token
