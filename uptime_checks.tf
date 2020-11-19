95c1049abe627a5153caf79ad9e95f920a31edff
Alexander Bredesen <alex474@hotmail.no>
2020-11-19 20:40:34 +0100

    changed contact group

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -7,5 +7,5 @@ resource "statuscake_test" "googlecloudruntest" {
   website_url = "${google_cloud_run_service.app.status[0].url}/stock"
   test_type = "HTTP"
   check_rate = 300
-  contact_group = ["Default"]
+  contact_group = ["Default Contact Group"]
 }
