662ba57dbc3bbd204e260533f5e63cff68be1961
Alexander Bredesen <alex474@hotmail.no>
2020-11-22 11:56:04 +0100

    travis encrypt username

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -1,11 +1,11 @@
 provider "statuscake" {
-  username = "alexanderbredesen98gmailcom"
+  version = "1.1.3"
 }
 
 resource "statuscake_test" "googlecloudruntest" {
   website_name = "exam"
-  website_url = "${google_cloud_run_service.app.status[0].url}/stock"
+  website_url = "${google_cloud_run_service.app.status[0].url}/stock" # Check against an endpoint
   test_type = "HTTP"
-  check_rate = 300
-  contact_group = ["Default"]
+  check_rate = 300 # How often the check should be runned
+  contact_group = ["Default"] # Who should be contacted
 }
